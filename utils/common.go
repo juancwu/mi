6517d53e0fb277f114d0eeedaf1401fcb09456d8
jc <46619361+juancwu@users.noreply.github.com>
2024-05-27 22:19:31 -0400

    update service url fn logic

--- utils/common.go
+++ utils/common.go
@@ -3,7 +3,7 @@ package utils
 import "os"
 
 func GetServiceURL() string {
-	if os.Getenv("APP_ENV") == "development" {
+	if os.Getenv("APP_ENV") != "production" {
 		return "http://127.0.0.1:3000"
 	}
 	return "https://konbini.juancwu.dev"
