e6ec1e87b0cbb1cab6f58c4a04e845a33b0a628e
jc <46619361+juancwu@users.noreply.github.com>
2024-11-10 18:33:49 -0500

    make version accessible through common pkg

--- main.go
+++ main.go
@@ -5,12 +5,15 @@ import (
 	"os"
 
 	"github.com/juancwu/mi/cmd"
+	"github.com/juancwu/mi/common"
 	"github.com/juancwu/mi/text"
 )
 
 var version string = "dev"
 
 func main() {
+	// make it accessible globally
+	common.Version = version
 	if err := cmd.Execute(version); err != nil {
 		fmt.Printf("%s\n", text.Foreground(text.RED, err.Error()))
 		os.Exit(1)
