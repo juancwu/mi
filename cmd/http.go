7cf8c893a2604682a0e59f72b0455b53fd93908f
jc <46619361+juancwu@users.noreply.github.com>
2024-12-13 02:11:55 -0500

    do-over

--- cmd/http.go
+++
@@ -1,8 +0,0 @@
-package cmd
-
-const (
-	header_content_type   = "Content-Type"
-	header_content_length = "Content-Length"
-	header_authorization  = "Authorization"
-	header_mime_json      = "application/json"
-)
